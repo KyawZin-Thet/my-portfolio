1e47753a28640ce96b35377ea40dacf18bacf86c
KyawZin-Thet <128166677+KyawZin-Thet@users.noreply.github.com>
2024-02-10 09:20:53 +0630

    upgraded

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
+  key: number;
   imageUrl: string;
   title: string;
+  description?: string;
   gitUrl: string;
   previewUrl: string;
 }
@@ -13,51 +15,63 @@ interface Props {
 export default function ProjectCard({
   imageUrl,
   title,
+  description,
   gitUrl,
   previewUrl,
+  key,
 }: Props) {
   return (
-    <div className="w-80 place-self-center border-white m-5">
-      <div className=" max-w-sm rounded-md overflow-hidden shadow-lg bg-slate-950">
-        <div className="h-60">
-          <Image
-            height={400}
-            width={200}
-            src={imageUrl}
-            alt={title}
-            style={{ width: "100%", height: "100%", borderRadius: "4px" }}
-          />
-        </div>
-        <div className="px-6 py-4 text-white">
-          <div className="h-10 text-lg mb-4">{title}</div>
-
-          <div className="flex justify-around mt-4">
-            <Link
-              href={gitUrl}
-              style={{
-                marginRight: "10px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <CodeIcon fontSize="large" />
-              <span className="text-center">View Code</span>
-            </Link>
-            <Link
-              style={{
-                marginRight: "10px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-              href={previewUrl}
-            >
-              <RemoveRedEyeIcon fontSize="large" />
-              <span className="text-center">Live View</span>
-            </Link>
+    <div
+      key={key}
+      className="m-8 max-w-sm w-full lg:max-w-full h-72 flex flex-wrap  "
+    >
+      <div className="hidden">
+        <Image
+          height={270}
+          width={280}
+          src={imageUrl}
+          alt={title}
+          style={{ borderRadius: "4px" }}
+        />
+      </div>
+      <div className="w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-#0ea5e9 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-4">
+          <div className="text-white font-bold text-xl my-2">{title}</div>
+          <div className=" mt-4 overflow-y-scroll ">
+            <p className="text-white text-base ">{description}</p>
           </div>
         </div>
+        {description && (
+          <div className="flex items-center">
+            <div className="flex justify-around mt-4 text-white">
+              <Link
+                href={gitUrl}
+                style={{
+                  marginRight: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <CodeIcon fontSize="large" />
+                <span className="text-center text-sm">View Code</span>
+              </Link>
+              <Link
+                style={{
+                  marginRight: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  marginLeft: 4,
+                }}
+                href={previewUrl}
+              >
+                <RemoveRedEyeIcon fontSize="large" />
+                <span className="text-center text-sm">Live View</span>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- app/components/ProjectSection.tsx
+++ app/components/ProjectSection.tsx
@@ -5,6 +5,8 @@ const projectsData = [
     id: 1,
     title: "Foodie pos",
     image: "/images/foodiePos.png",
+    description:
+      "This is a POS software that facilitates the creation and updating of menus. It encompasses both Backoffice and order app functionalities, supporting CRUD operations, Nextauth Google login, file uploads on AWS space using multerS3, and is deployed on Vercel, utilizing the Render database. The tech stack is > Next.JS,React, MaterialUI, Redux Toolkit,PostgreSQL",
     gitUrl: "https://github.com/KyawZin-Thet/my-foodie-pos",
     previewUrl: "https://my-foodie-pos-eight.vercel.app/",
   },
@@ -12,6 +14,8 @@ const projectsData = [
     id: 2,
     title: "Snakes",
     image: "/images/snakesProject.jpg",
+    description:
+      "This is a front-end project about snakes. You can search for snakes by name and click to see details about the snake.The tech stack is > Next.JS,React, MaterialUI, Redux Toolkit",
     gitUrl: "https://github.com/KyawZin-Thet/snakes",
     previewUrl: "https://snakes-rose.vercel.app/",
   },
@@ -19,6 +23,8 @@ const projectsData = [
     id: 3,
     title: "Travel-Guide",
     image: "/images/travelguide.jpg",
+    description:
+      "This project was created by two developers. It revolves around travel, allowing users to find the city they want to visit. Users can click to view popular places in that city, as well as information about buses and airlines. Additionally, users have the ability to send messages to admins. On the admin side, CRUD operations are implemented, enabling the addition of new cities, places, buses, and airlines. The tech stack is > Next.JS,React, MaterialUI, Redux Toolkit,PostgreSQL",
     gitUrl: "https://github.com/NyiWaingchit7/travel-NextJs-with-Kyaw-Zin-Thet",
     previewUrl: "https://travel-next-js-with-kyaw-zin-thet.vercel.app/",
   },
@@ -27,6 +33,8 @@ const projectsData = [
     id: 4,
     title: "More Projects Are Coming! ",
     image: "/images/comingsoon.jpg",
+    description:
+      "As I enjoy coding, I love creating new projects with improved ideas and skills.",
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -34,15 +42,16 @@ const projectsData = [
 
 export default function ProjectSection() {
   return (
-    <section className="my-7" id="projects">
+    <section className="mt-7 mb-28" id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:gap-12">
+      <ul className="flex justify-center flex-wrap w-full">
         {projectsData.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
+            description={project.description}
             imageUrl={project.image}
             gitUrl={project.gitUrl}
             previewUrl={project.previewUrl}
