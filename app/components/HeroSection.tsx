d0f1af3b263c76c4f0048e3d78d879ae29d0d63d
KyawZin-Thet <128166677+KyawZin-Thet@users.noreply.github.com>
2024-02-11 23:31:53 +0630

    upgraded 2

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,11 +4,11 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function HeroSection() {
   return (
-    <section className="flex items-center mt-32 sm:mt-0  h-lvh">
-      <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center ">
+    <section className="flex items-center mt-0   h-lvh">
+      <div className="grid grid-cols-1  sm:grid-cols-2 place-items-center ">
         <div className=" text-center sm:text-left ">
-          <div className="mb-6 pt-1 h-32 ">
-            <h1 className="hidden sm:block sm:pt-3 mt-5 text-white text-2xl sm:text-3xl lg:leading-normal font-extrabold">
+          <div className=" mb-2 pt-1 h-28 sm:h-44 ">
+            <h1 className="hidden sm:block sm:pt-3 mt-5 text-white sm:text-3xl lg:leading-normal font-extrabold">
               Hello I&apos;m
             </h1>
             <br />
@@ -16,12 +16,12 @@ export default function HeroSection() {
               sequence={["Kyaw Zin Thet", 1800, "Junior Web-developer", 1800]}
               wrapper="span"
               speed={40}
-              className=" text-white text-3xl sm:text-4xl lg:leading-normal font-extrabold "
+              className=" text-white text-2xl md:text-4xl lg:leading-normal font-extrabold "
               repeat={Infinity}
             />
           </div>
 
-          <p className="text-white p-5 text-base sm:text-lg my-5 lg:text-xl">
+          <p className="text-white text-base sm:text-lg my-5 lg:text-xl">
             I am actively seeking a position as a developer. I am a young and
             hardworking individual, enthusiastic about learning and willing to
             accept a competitive salary.
